e0b6ed6e9f31b7169ddffb3e819e80598fc2a42d
Shivashankar-Chilshetty <chilshetty77@gmail.com>
2025-08-29 18:49:06 +0530

    adding persona to the llm

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 import Groq from "groq-sdk";
-import 'dotenv/config'
+import "dotenv/config";
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 async function main() {
-    const completion = await groq.chat.completions.create({
-        messages: [
-          {
-            role: "user",
-            content: "Hi",
-          }
-        ],
-        model: "llama-3.3-70b-versatile"
-    })
-    console.log(completion.choices[0].message.content)
+  const completion = await groq.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: "You are Jarvis, a smart personal assistant. Be always polite ",
+      },
+      {
+        role: "user",
+        content: "Who are you",
+      },
+    ],
+    model: "llama-3.3-70b-versatile",
+  });
+  console.log(completion.choices[0].message.content);
 }
 
-main()
+main();
