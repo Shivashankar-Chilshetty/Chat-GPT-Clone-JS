acfe838f0e8f3f23261ec2fc2ff72a478bc96c25
Shivashankar-Chilshetty <chilshetty77@gmail.com>
2025-08-29 18:42:01 +0530

    initial setup for chat-gpt-clone

---
+++ app.js
@@ -0,0 +1,18 @@
+import Groq from "groq-sdk";
+import 'dotenv/config'
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+
+async function main() {
+    const completion = await groq.chat.completions.create({
+        messages: [
+          {
+            role: "user",
+            content: "Hi",
+          }
+        ],
+        model: "llama-3.3-70b-versatile"
+    })
+    console.log(completion.choices[0].message.content)
+}
+
+main()
